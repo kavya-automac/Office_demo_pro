d397675b42dea7e0f4ba3c81161750fffc616f25
Harika-Setava <Harika-Setava>
2025-02-19 13:16:23 +0530

    working pro

--- office_emppro/office_emppro/settings.py
+++ office_emppro/office_emppro/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'office_emppro.urls'
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'office_emppro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testuser',
-        'USER': 'postgres',
-        'PASSWORD':'1357',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
