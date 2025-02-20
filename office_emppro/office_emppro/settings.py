e47a9de93429602f877b4956c116f5f691c9bd3a
Harika-Setava <Harika-Setava>
2025-02-20 16:32:47 +0530

    added aws ipaddress

--- office_emppro/office_emppro/settings.py
+++ office_emppro/office_emppro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3_3^#8qkbzf0+x(3v+w9c%0%32e(o=_7m%s_3$1p-ho)2+@ziw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com",'127.0.0.1','192.168.29.144','13.53.212.0']
+ALLOWED_HOSTS = [".awsapprunner.com",'127.0.0.1','192.168.29.144','13.53.212.0','52.66.253.151']
 
 # Application definition
 
