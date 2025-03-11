46dba1890ae185bc9b93b43d7e1b6a38fe3bac85
Harika-Setava <Harika-Setava>
2025-03-11 16:35:50 +0530

    added static root

--- office_emppro/office_emppro/settings.py
+++ office_emppro/office_emppro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,8 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/var/www/OfficeDemo/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'build/static')
+]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     "default": {
